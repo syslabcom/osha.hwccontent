9303fb6d167c6c77ccf80ee1a490acde86b6de88
Wolfgang Thomas <thomas@syslab.com>
2013-09-26 18:05:26 +0200

    added more fields and in the correct order for #8436

--- src/osha/hwccontent/organisation.py
+++ src/osha/hwccontent/organisation.py
@@ -6,135 +6,277 @@ from plone.supermodel import model
 from zope import schema
 from plone.multilingualbehavior import directives
 
+from plone.directives import dexterity
+from plone.directives import form
 
-class IOrganisation(model.Schema):
+
+class IOrganisationPhase1(model.Schema):
+
+    # Countries of Activity
+    form.fieldset(
+        'about_organisation',
+        label=_(u'About your organisation'),
+        fields=[
+            'countries', 'additional_countries', 'title', 'organisation_type',
+            'business_sector', 'social_dialogue', 'street', 'address_extra',
+            'city', 'zip_code', 'country', 'email', 'phone', 'fax', 'key_name',
+            'key_position', 'key_email', 'key_phone', 'url', 'campaign_url',
+        ],
+    )
+
+    countries = schema.List(
+        title=_(u"Countries of activity"),
+        description=_(
+            u"Only Pan-European or international organisations / "
+            "companies are eligible for the European Campaign Partnership; "
+            "please indicate the countries your company is represented through"
+            " branches / representative offices / network members"),
+        value_type=schema.Choice(
+            vocabulary=vocabularies.countries,
+        ),
+    )
+    directives.languageindependent('countries')
+
+    additional_countries = schema.Text(
+        title=_(u"Other countries"),
+        description=_(
+            u"Other countries in which you organisation is represented (enter "
+            "names)"),
+        required=False,
+    )
+    directives.languageindependent('additional_countries')
+
+    # Organisation's profile and contact details
+
+    title = schema.TextLine(
+        title=_(u"Company / Organisation name")
+    )
+
+    organisation_type = schema.Choice(
+        title=_(u"Organisation type"),
+        vocabulary=vocabularies.organisation_types,
+    )
+    directives.languageindependent('organisation_type')
+
+    business_sector = schema.TextLine(
+        title=_(u"Business sector"),
+        description=_(u"Business sector in which you operate"),
+    )
+    directives.languageindependent('business_sector')
+
+    social_dialogue = schema.Choice(
+        title=_(u"Social Dialogue Partner?"),
+        description=_(
+            u"Are you a Social Partner within the framework of the European "
+            "Social Dialogue?"),
+        vocabulary=vocabularies.yes_no,
+    )
 
     street = schema.TextLine(
-        title = _(u"Street"),
+        title=_(u"Address"),
+        description=_(u"Street or PO-box")
     )
     directives.languageindependent('street')
 
     address_extra = schema.TextLine(
+        title=_(u"Address extra"),
         required=False,
-        title = _(u"Address extra"),
     )
     directives.languageindependent('address_extra')
 
     city = schema.TextLine(
-        title = _(u"City"),
+        title=_(u"City"),
     )
     directives.languageindependent('city')
 
     zip_code = schema.TextLine(
-        title = _(u"Zip code"),
+        title=_(u"Zip code"),
     )
     directives.languageindependent('zip_code')
 
-    country = schema.Choice(
-        title = _(u"Country"),
-        vocabulary = vocabularies.countries,
+    country = schema.TextLine(
+        title=_(u"County"),
     )
     directives.languageindependent('country')
 
     email = schema.TextLine(
-        title = _(u"E-Mail"),
+        title=_(u"General company email address"),
     )
     directives.languageindependent('email')
 
     phone = schema.TextLine(
-        title = _(u"Telephone number"),
+        title=_(u"General telephone number"),
     )
     directives.languageindependent('phone')
 
     fax = schema.TextLine(
         required=False,
-        title = _(u"Fax number"),
+        title=_(u"General fax number"),
     )
     directives.languageindependent('fax')
 
-    url = schema.TextLine(
-        title = _(u"Website"),
-    )
-    directives.languageindependent('url')
-
-    campaign_url = schema.TextLine(
-        title = _(u"Dedicated Campaign Website"),
-    )
-    directives.languageindependent('campaign_url')
-
-    logo = NamedBlobImage(
-        title = _(u"Company/Organisation logo"),
-    )
-    directives.languageindependent('logo')
-
-    organisation_type = schema.TextLine(
-        title = _(u"Organisation type"),
-    )
-
-    business_sector = schema.TextLine(
-        title = _(u"Business sector"),
-    )
-
-    mission_statement = schema.Text(
-        title = _(u"Mission statement"),
-    )
-
-    campaign_pledge = schema.Text(
-        title = _(u"Our Campaign Pledge"),
-    )
-
-    ceo_image = NamedBlobImage(
-        title = _(u"Photo of your CEO, President, General Director or other"),
-    )
-    directives.languageindependent('ceo_image')
-
-    ceo_name = schema.TextLine(
-        description = _(u"Name / surname of your CEO, President, General Director or other"),
-        title = _(u"CEO"),
-    )
-    directives.languageindependent('ceo_name')
-
-    ceo_position = schema.TextLine(
-        description = _(u"Please indicate the actual position, such as President, General Director, CEO, Chairman, etc"),
-        title = _(u"Position identifier"),
-    )
-    directives.languageindependent('ceo_position')
+    # Organisation's contact person
 
     key_name = schema.TextLine(
-        description = _(u"Name/Surname of main contact person for the Campaign"),
-        title = _(u"Contact person"),
+        description=_(u"Name/Surname of main contact person for the Campaign"),
+        title=_(u"Contact person"),
     )
     directives.languageindependent('key_name')
 
     key_position = schema.TextLine(
-        title = _(u"Position of the main contact person."),
+        title=_(u"Position of the main contact person."),
     )
     directives.languageindependent('key_position')
 
     key_email = schema.TextLine(
-        title = _(u"Email address of main contact person."),
+        title=_(u"Email address of main contact person."),
     )
     directives.languageindependent('key_email')
 
     key_phone = schema.TextLine(
-        title = _(u"Telephone number of main contact person."),
+        title=_(u"Telephone number of main contact person."),
     )
     directives.languageindependent('key_phone')
 
+    # Organisation's website
+
+    url = schema.TextLine(
+        title=_(u"Home page"),
+        description=_("Organisation's website"),
+    )
+    directives.languageindependent('url')
+
+    campaign_url = schema.TextLine(
+        title=_(u"Dedicated Campaign Website"),
+        description=_(
+            u'Special web section dealing with issues related to occupational '
+            'health and safety'),
+        required=False,
+    )
+    directives.languageindependent('campaign_url')
+
+    # About your involvement in the Campaign
+    form.fieldset(
+        'campaign_involvement',
+        label=_(u"About your involvement in the campaign"),
+        fields=[
+            'why_partner', 'promotion_electronic']
+    )
+
+    why_partner = schema.Text(
+        title=_(
+            u"1. Why do you want to be an official partner of the Healthy "
+            u"Workplaces Campaign 2014-15 on ‘Healthy workplaces manage "
+            u"stress’?"),
+        description=_(u"Please explain briefly"),
+    )
+    directives.languageindependent('why_partner')
+
+    promotion_electronic = schema.Text(
+        title=_(
+            u"2. How do you plan to promote and support the Campaign via your "
+            "corporate website and in any other electronic means (intranet, "
+            "newsletter, social media, etc.)?"),
+        description=_(u"Please describe"),
+        required=False,
+    )
+    directives.languageindependent('promotion_electronic')
+
+    # To be continued...
+    # promotion_other = schema.Text(
+    #     title=_(
+    #         u"")
+    # )
+
+
+class IOrganisationPhase2(model.Schema):
+
+    form.fieldset(
+        'additional_information',
+        label=_(u"Additional information"),
+        fields=[
+            'mission_statement', 'ceo_name', 'ceo_position', 'ceo_quote',
+            'representative_name', 'representative_email',
+            'representative_phone', 'logo', 'ceo_image', 'campaign_pledge']
+    )
+
+    mission_statement = schema.Text(
+        title=_(u"Your mission statement"),
+        description=_(u"Briefly outline the mission of your company"),
+    )
+    directives.languageindependent('mission_statement')
+
+    ceo_name = schema.TextLine(
+        title=_(u"CEO"),
+        description=_(
+            u"Name / surname of your CEO, President, General Director or "
+            "other"),
+    )
+    directives.languageindependent('ceo_name')
+
+    ceo_position = schema.TextLine(
+        title=_(u"Position identifier"),
+        description=_(
+            u"Please indicate the actual position, such as President, General "
+            "Director, CEO, Chairman, etc"),
+        default=u"CEO",
+    )
+    directives.languageindependent('ceo_position')
+
+    ceo_quote = schema.TextLine(
+        title=_(
+            u'His / her quote on the Healthy Workplaces Campaign on "Working '
+            'together for risk prevention"'),
+    )
+    directives.languageindependent('ceo_quote')
+
     representative_name = schema.TextLine(
         required=False,
-        title = _(u"Name of your organisation\'s health and safety representative"),
+        title=_(
+            u"Name of your organisation's health and safety representative"),
     )
     directives.languageindependent('representative_name')
 
     representative_email = schema.TextLine(
         required=False,
-        title = _(u"Email address of your organisation\'s health and safety representative"),
+        title=_(
+            u"Email address of your organisation's health and safety "
+            "representative"),
     )
     directives.languageindependent('representative_email')
 
     representative_phone = schema.TextLine(
         required=False,
-        title = _(u"Telephone number of your organisation's health and safety representative"),
+        title=_(
+            u"Telephone number of your organisation's health and safety "
+            "representative"),
     )
     directives.languageindependent('representative_phone')
+
+    logo = NamedBlobImage(
+        title=_(u"Company / Organisation logo"),
+        description=_(
+            u"Please add an image with the company / organisation logo. "
+            u"Please use a format suited for web display if possible (JPEG, "
+            u"PNG or GIF).")
+    )
+    directives.languageindependent('logo')
+
+    ceo_image = NamedBlobImage(
+        title=_(u"Photo of your CEO, President, General Director or other"),
+    )
+    directives.languageindependent('ceo_image')
+
+    campaign_pledge = schema.Text(
+        title=_(
+            u"How does your organisation plan to contribute to making this "
+            u"Campaign a joint success? (your Campaign pledge)"),
+        description=_(
+            u"Please summarise briefly the support that you will provide "
+            u"described under STEP 2 of this application form (max. 150 "
+            u"words)."),
+    )
+
+
+class IOrganisation(IOrganisationPhase1):
+    pass
--- src/osha/hwccontent/vocabularies.py
+++ src/osha/hwccontent/vocabularies.py
@@ -1,15 +1,34 @@
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from osha.hwccontent import _
 
-COUNTRIES = [#'Choose yours ...',
-        'Austria', 'Belgium', 'Bulgaria', 'Cyprus', 
-        'Czech Republic', 'Denmark', 'Estonia', 'Finland', 
-        'France', 'Germany', 'Greece', 'Hungary', 'Iceland',
-        'Ireland', 'Italy', 'Latvia', 'Liechtenstein', 'Lithuania', 
-        'Luxembourg', 'Malta', 'Netherlands', 'Norway', 'Poland', 
-        'Portugal', 'Romania', 'Slovak Republic', 'Slovenia', 
-        'Spain', 'Sweden', 'Switzerland', 'United Kingdom']
+COUNTRIES = [
+    'Austria', 'Belgium', 'Bulgaria', 'Cyprus',
+    'Czech Republic', 'Denmark', 'Estonia', 'Finland',
+    'France', 'Germany', 'Greece', 'Hungary', 'Iceland',
+    'Ireland', 'Italy', 'Latvia', 'Liechtenstein', 'Lithuania',
+    'Luxembourg', 'Malta', 'Netherlands', 'Norway', 'Poland',
+    'Portugal', 'Romania', 'Slovak Republic', 'Slovenia',
+    'Spain', 'Sweden', 'Switzerland', 'United Kingdom'
+]
 
 countries = SimpleVocabulary(
     [SimpleTerm(value=item, title=_(item)) for item in COUNTRIES]
-    )
+)
+
+ORG_TYPES = [
+    'Private company', 'Trade union', 'Employer organisation',
+    'OSH professional organisation', 'Research organisation', 'Other']
+
+organisation_types = SimpleVocabulary(
+    [SimpleTerm(value=item, title=_(item)) for item in ORG_TYPES]
+)
+
+YES_NO = [
+    ("Please select", None),
+    ("Yes", True),
+    ("No", False),
+]
+
+yes_no = SimpleVocabulary(
+    [SimpleTerm(value=item[1], title=_(item[0])) for item in YES_NO]
+)
