6e833ae3f6f70dc72f1fc0c3faeb1715aa230be2
Cillian de Roiste <deroiste@syslab.com>
2013-10-04 16:44:44 +0000

    Refs #8734 Display news feed on custom front page view

---
+++ src/osha/hwccontent/browser/front_page.py
@@ -0,0 +1,26 @@
+# _+- coding: utf-8 -*-
+
+from Acquisition import aq_parent
+from Products.Five.browser import BrowserView
+from plone import api
+
+
+class FrontPageView(BrowserView):
+
+    def _get_feed_contents(self, feed_path_from_nav_root=""):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        nav_root = api.portal.get_navigation_root(self.context)
+        feed_folder_path = nav_root.absolute_url_path()\
+                           + feed_path_from_nav_root
+        num_results = 2
+        return catalog.searchResults(
+            path=feed_folder_path,
+            portal_type="FeedFeederItem",
+            sort_limit=num_results,
+            sort_on="effective",
+            sort_order="descending",
+        )[:num_results]
+
+    @property
+    def news(self):
+        return self._get_feed_contents("/news/combined-feed-folder/")
