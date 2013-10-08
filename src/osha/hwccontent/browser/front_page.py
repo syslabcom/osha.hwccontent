4e1d0c54fda20050a1588c2565afcffa821dece6
Cillian de Roiste <deroiste@syslab.com>
2013-10-08 13:36:03 +0000

    Refs #8807 Show latest news and events on the front page

--- src/osha/hwccontent/browser/front_page.py
+++ src/osha/hwccontent/browser/front_page.py
@@ -12,27 +12,25 @@ except ImportError:
 
 class FrontPageView(BrowserView):
 
-    def _get_feed_contents(self, feed_path_from_nav_root=""):
+    def news(self):
         catalog = api.portal.get_tool(name='portal_catalog')
-        nav_root = api.portal.get_navigation_root(self.context)
-        feed_folder_path = nav_root.absolute_url_path()\
-                           + feed_path_from_nav_root
         num_results = 2
         return catalog.searchResults(
-            path=feed_folder_path,
-            portal_type="FeedFeederItem",
+            portal_type="News Item",
             sort_limit=num_results,
             sort_on="effective",
             sort_order="descending",
         )[:num_results]
 
-    @property
-    def news(self):
-        return self._get_feed_contents("/news/combined-feed-folder/")
-
-    @property
     def events(self):
-        return self._get_feed_contents("/events/combined-feed-folder/")
+        catalog = api.portal.get_tool(name='portal_catalog')
+        num_results = 2
+        return catalog.searchResults(
+            portal_type="Event",
+            sort_limit=num_results,
+            sort_on="start",
+            sort_order="descending",
+        )[:num_results]
 
     def partners(self):
         catalog = api.portal.get_tool(name='portal_catalog')
