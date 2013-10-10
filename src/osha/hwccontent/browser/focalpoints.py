ffa4a2f54bb285beb378a91c3168243b25ff8e58
Cillian de Roiste <deroiste@syslab.com>
2013-10-10 15:07:17 +0000

    Refs #8821 Add view for focal points, simplified version of the org view

---
+++ src/osha/hwccontent/browser/focalpoints.py
@@ -0,0 +1,13 @@
+# _+- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
+from five import grok
+from osha.hwccontent.focalpoint import IFocalPoint
+from osha.hwccontent.browser.organisation import View as OrganisationView
+
+grok.templatedir("templates")
+
+
+class View(OrganisationView):
+    grok.context(IFocalPoint)
+    grok.require('zope2.View')
+    grok.template("focalpoint")
