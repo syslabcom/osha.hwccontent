c4efab6e884a028444cd554d747a3402946b85ba
Wolfgang Thomas <thomas@syslab.com>
2013-10-21 19:14:50 +0200

    set class "subsection" on all items under a SectionIntro

--- src/osha/hwccontent/browser/theming.py
+++ src/osha/hwccontent/browser/theming.py
@@ -1,9 +1,27 @@
 # -*- coding: utf-8 -*-
+from Acquisition import aq_parent
+from Products.CMFPlone.utils import getDefaultPage
 from osha.hwccontent.interfaces import ITwoImages, ISectionIntro
 from plone.app.layout.globals import layout
+from plone.app.layout.navigation.interfaces import INavigationRoot
 
 
 class LayoutPolicy(layout.LayoutPolicy):
+
+    def is_subsection(self, context, view):
+        # if isDefaultPage(context):
+        #
+        if ISectionIntro.providedBy(context) or ISectionIntro.providedBy(view):
+            return False
+        while not INavigationRoot.providedBy(context):
+            context = aq_parent(context)
+            if ISectionIntro.providedBy(context):
+                return True
+            default_page = getDefaultPage(context, self.request)
+            if default_page and ISectionIntro.providedBy(
+                    getattr(context, default_page)):
+                return True
+        return False
 
     def bodyClass(self, template, view):
         """Returns the CSS class to be used on the body tag."""
@@ -23,5 +41,8 @@ class LayoutPolicy(layout.LayoutPolicy):
         # the marker interface can also be set on the view
         if ISectionIntro.providedBy(view):
             body_class += " section-intro"
+
+        if self.is_subsection(self.context, view):
+            body_class += " subsection"
 
         return body_class
