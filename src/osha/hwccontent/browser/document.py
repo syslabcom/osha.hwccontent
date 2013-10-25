c2d9c8f98b843846e4160667e85ae7fa27791411
Lennart Regebro <regebro@gmail.com>
2013-10-25 18:02:02 +0200

    Ugly test for #8958, commited so I can continue on the laptop.

--- src/osha/hwccontent/browser/document.py
+++ src/osha/hwccontent/browser/document.py
@@ -1,11 +1,17 @@
 # _+- coding: utf-8 -*-
 
-from Acquisition import aq_parent
-from Products.Five.browser import BrowserView
-from osha.hwccontent.browser.utils import get_partners, css_by_orientation
-from osha.hwccontent.interfaces import IFullWidth
+from datetime import datetime
+from json import load
+from urllib import urlopen
+from urlparse import urljoin
+
 from plone import api
 from zope.interface import implements
+from Acquisition import aq_parent
+from Products.Five.browser import BrowserView
+
+from osha.hwccontent.browser.utils import get_partners, css_by_orientation
+from osha.hwccontent.interfaces import IFullWidth
 
 try:
     from collections import OrderedDict
@@ -56,3 +62,34 @@ class FocalPointsView(BrowserView):
                 letters[letter] = {}
             letters[letter][country] = fop.absolute_url()
         return letters
+
+class PressReleaseView(BrowserView):
+    implements(IFullWidth)
+
+    def pressreleases(self):
+        remote_url = 'http://localhost:8080/osha/portal'
+        qurl = urlopen(remote_url + '/jsonfeed?portal_type=PressRelease&path=/de/press/press-releases&subject=stress&language=de')
+        for item in load(qurl):
+            yield {
+                'title': item['title'],
+                'releaseDate': datetime.strptime(item['releaseDate'].split('+')[0], '%Y-%m-%dT%H:%M:%S'),
+                'url': urljoin(remote_url, item['_path']),
+            }
+        
+        
+        
+        
+    #[u'relatedLinks', u'referenced_content', u'subhead', u'contributors',
+     #u'text', u'image', u'creation_date', u'releaseDate', u'imageCaption',
+     #u'expirationDate', u'reindexTranslations', u'_image_filename',
+     #u'notesToEditors', u'id', u'subject', u'_image_content_type',
+     #u'effectiveDate', u'title', u'_text_content_type', u'relatedItems',
+     #u'location', u'releaseTiming', u'_seoDescription_content_type',
+     #u'excludeFromNav', u'showinsearch', u'_type', u'description',
+     #u'searchwords', u'showContacts', u'_searchwords_content_type',
+     #u'osha_metadata', u'_rights_content_type', u'_description_content_type',
+     #u'modification_date', u'language', u'releaseContacts', u'country',
+     #u'rights', u'isNews', u'allowDiscussion', u'seoDescription', u'creators',
+     #u'pdfShowTitle', u'_path']
+
+    
