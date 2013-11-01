78c968bcc3f7eeb9d4021337f28fc3191eb5af0e
Lennart Regebro <regebro@gmail.com>
2013-11-01 14:51:56 +0100

    Move the press release listing over to use the same handy base class as the otehr remote listings. Ref: #8958

--- src/osha/hwccontent/browser/pressrelease.py
+++ src/osha/hwccontent/browser/pressrelease.py
@@ -7,8 +7,9 @@ from osha.hwccontent.browser.utils import isotime2dt
 from osha.hwccontent.interfaces import IFullWidth
 from plone import api
 from plone.app.contenttypes.interfaces import ICollection
-from plone.app.event.base import date_speller
 from plone.app.querystring.querybuilder import QueryBuilder
+from plone.app.textfield.interfaces import ITransformer
+from plone.app.textfield.value import RichTextValue
 from plone.memoize import ram
 from urllib import urlopen
 from zope.component import getMultiAdapter
@@ -17,23 +18,22 @@ from zope.interface import implements
 
 import base64
 
-
-class EventListing(ListingView):
+class PressReleaseListing(ListingView):
     implements(IFullWidth)
 
 
     def __init__(self, context, request):
-        super(EventListing, self).__init__(context, request)
+        super(PressReleaseListing, self).__init__(context, request)
         properties = api.portal.get_tool('portal_properties')
         self.osha_json_url = getattr(
             properties.site_properties,
             'osha_json_url',
             'https://osha.europa.eu/'
         )
-        self.remote_event_query_tags = getattr(
+        self.remote_press_release_query_tags = getattr(
             properties.site_properties,
-            'remote_event_query_tags',
-            'stress'
+            'remote_press_release_query_tags',
+            'Campaign%202012-13'
         )
 
     def results(self, collection, batch=True, b_start=0, b_size=None,
@@ -55,27 +55,34 @@ class EventListing(ListingView):
             limit=limit, brains=brains
         )
     
-    @ram.cache(ListingView.cache_for_minutes(10))
-    def get_remote_events(self):
-        """ Queries the OSHA corporate site for events.
+    #@ram.cache(ListingView.cache_for_minutes(10))
+    def get_remote_press_releases(self):
+        """ Queries the OSHA corporate site for press releases.
             Items returned in JSON format.
         """
         items = []
         lang = api.portal.get_tool("portal_languages").getPreferredLanguage()
-        qurl = '%s/%s/jsonfeed?portal_type=Event&Subject=%s&path=/osha/portal/%s&Language=%s' \
-            % (self.osha_json_url, lang, self.remote_event_query_tags, lang, lang)
+        qurl = '%s/%s/jsonfeed?portal_type=PressRelease&Subject=%s&path=/osha/portal/%s&Language=%s' \
+            % (self.osha_json_url, lang, self.remote_press_release_query_tags, lang, lang)
 
         result = urlopen(qurl)
         if result.code == 200:
             for item in load(result):
-                item['start'] = DateTime(item['startDate'])
-                item['end'] = DateTime(item['endDate'])
-                
-                items.append(item)
+                items.append({
+                    'remote_item': True,
+                    'Title': item['title'],
+                    'Date': item['effectiveDate'],
+                    'getURL': item['_url'],
+                    'Description': item.get('description', ''),
+                    'image_base64': item.get('image'),
+                    'image_content_type': item.get('image_type'),
+                    'text': self.make_intro(self.make_plain_text(item)),
+                    
+                })
         return items
 
-    @ram.cache(ListingView.cache_for_minutes(1))
-    def get_local_events(self):
+    #@ram.cache(ListingView.cache_for_minutes(1))
+    def get_local_press_releases(self):
         """ Looks in the current folder for Collection objects and then queries
             them for items.
         """
@@ -85,43 +92,56 @@ class EventListing(ListingView):
                 items = self.results(
                     child,
                     batch=False,
-                    sort_on='start',
+                    sort_on='Date',
                     brains=True)
         return items
 
-    def get_all_events(self):
-        items = self.get_remote_events() + \
-            list(self.get_local_events())
+    def get_all_press_releases(self):
+        items = self.get_remote_press_releases() + \
+            list(self.get_local_press_releases())
         return sorted(
             items,
-            key=lambda item: item.__getitem__('start'),
+            key=lambda item: item.__getitem__('Date'),
             reverse=True
         )
 
-    def get_batched_events(self):
+    def get_batched_press_releases(self):
         b_size = int(self.request.get('b_size', 20))
         b_start = int(self.request.get('b_start', 0))
-        items = self.get_all_events()
+        items = self.get_all_press_releases()
         for i in range(b_start, b_size):
             if i >= len(items):
                 break
             if ICatalogBrain.providedBy(items[i]):
                 item = items[i]
                 obj = item.getObject()
+                blob = getattr(obj.image, '_blob', None)
+                plain_text = obj.restrictedTraverse('@@text-transform/text/text/plain')
                 items[i] = {
-                    'title': item.Title,
-                    'start': item.start,
-                    'end': item.end,
-                    '_url': item.getURL(),
-                    'description': item.Description,
-                    'obj': obj
+                    'Title': item.Title,
+                    'Date': DateTime(item.Date).utcdatetime(),
+                    'getURL': item.getPath(),
+                    'Description': item.Description,
+                    'image': blob and base64.encodestring(blob.open().read()) or None,
+                    'obj': obj,
+                    'text': self.make_intro(plain_text),
                 }
+            else: 
+                items[i]['Date'] = DateTime(items[i]['Date']).utcdatetime()
         return Batch(items, b_size, b_start, orphan=1)
 
-    def date_speller(self, date):
-        return date_speller(self.context, date)
+    def make_plain_text(self, item):
+        text = item.get('text')
+        if not text:
+            return ''
 
-    def formatted_date(self, occ):
-        provider = getMultiAdapter((self.context, self.request, self),
-                IContentProvider, name='formatted_date')
-        return provider(occ.context)
+        transformer = ITransformer(self.context)
+        value = RichTextValue(text, mimeType=item.get('_text_mime_type', 'text/html'))
+        return transformer(value, 'text/plain')
+        
+    def make_intro(self, text):
+        if len(text) < 200:
+            return text
+        
+        text = text[:200].rsplit(None, 1)[0]
+        return text + u'...'
