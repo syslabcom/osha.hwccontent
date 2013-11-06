7dba68b2f50048502ad8a312d5fdc6c624d5cb6b
Wolfgang Thomas <thomas@syslab.com>
2013-11-06 11:36:07 +0100

    add possibility for a "disclaimer" on the materials listing

--- src/osha/hwccontent/browser/folder.py
+++ src/osha/hwccontent/browser/folder.py
@@ -1,6 +1,7 @@
 from Products.CMFCore.interfaces import IFolderish
 from osha.hwccontent.interfaces import IMaterialsView
 from osha.hwccontent.browser.helper import get_path_to_icon
+from plone.app.textfield.interfaces import IRichTextValue
 from five import grok
 from zope import interface
 
@@ -18,3 +19,14 @@ class MaterialsView(grok.View):
 
     def get_icon_path(self, obj, content_type=None):
         return get_path_to_icon(obj, content_type)
+
+    @property
+    def disclaimer(self):
+        item = getattr(self.context, 'disclaimer', None)
+        value = item and getattr(item, 'text', None)
+        if value:
+            if IRichTextValue.providedBy(value):
+                return value.output
+            else:
+                return value
+        return ""
