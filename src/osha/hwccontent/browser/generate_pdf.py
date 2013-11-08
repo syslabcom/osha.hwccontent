9858e20705df827cd46dff1f267ea66f1a9435fd
Cillian de Roiste <deroiste@syslab.com>
2013-11-08 14:17:44 +0000

    generatePDF: copy the version from osha.hw utils instead of the external method
    It seems to be more recent

--- src/osha/hwccontent/browser/generate_pdf.py
+++ src/osha/hwccontent/browser/generate_pdf.py
@@ -1,6 +1,6 @@
 import os, tempfile
 from StringIO import StringIO
-from reportlab.lib.pagesizes import A4
+from reportlab.lib.pagesizes import A4, landscape
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.units import cm
 from reportlab.lib.utils import ImageReader
@@ -17,6 +17,7 @@ import xml.sax.saxutils as saxutils
 reportlab.rl_config.warnOnMissingFontGlyphs = 0
 
 def generatePDF(self,
+
         company="European Agency for Safety and Health at Work",
         language='en',
         firstname='',
@@ -31,7 +32,7 @@ def generatePDF(self,
                 'el', 'en', 'es', 'et',
                 'fi', 'fr', 'hu', 'it',
                 'lt', 'lv', 'mt', 'nl',
-                'pl', 'ro', 'pt', 'sk',
+                'pl', 'ro', 'pt', 'sk', 'no', 'is',
                 'sl', 'sv']:
         language = "en"
 
@@ -40,34 +41,35 @@ def generatePDF(self,
 
     # create a canvas and set metadata
     acknowledge = StringIO()
-    my_canvas = canvas.Canvas(acknowledge, pagesize=A4)
+    my_canvas = canvas.Canvas(acknowledge, pagesize=landscape(A4))
     my_canvas.setTitle('Charter of the Healthy Workplaces campaign')
     my_canvas.setAuthor('European Agency for Safety and Health at Work')
     my_canvas.setSubject('Charter of the Healthy Workplaces campaign')
 
     # register the font for writing company, name and actions
-    arial =  INSTANCE_HOME + '/../../data/arial.ttf'
+    pwd = os.path.dirname(__file__)
+    font_dir = os.path.realpath(os.path.join(pwd, "../resources/fonts"))
+    arial = font_dir + '/arial.ttf'
     pdfmetrics.registerFont( TTFont('Arial', arial) )
-    arial_bold =  INSTANCE_HOME + '/../../data/arialbd.ttf'
+    arial_bold =  font_dir + '/arialbd.ttf'
     pdfmetrics.registerFont( TTFont('ArialBold', arial_bold) )
-    arial_italic =  INSTANCE_HOME + '/../../data/ariali.ttf'
+    arial_italic =  font_dir + '/ariali.ttf'
     pdfmetrics.registerFont( TTFont('ArialItalic', arial_italic) )
-    arial_bi =  INSTANCE_HOME + '/../../data/arialbi.ttf'
+    arial_bi =  font_dir + '/arialbi.ttf'
     pdfmetrics.registerFont( TTFont('ArialBoldItalic', arial_bi) )
-    arial_nb =  INSTANCE_HOME + '/../../data/arialnb.ttf'
+    arial_nb =  font_dir + '/arialnb.ttf'
     pdfmetrics.registerFont( TTFont('ArialNarrowBold', arial_nb) )
-
-    # get the frontimage and write it to the canvas
-    charterfilename = "charter_hw2010_%s.jpg"
-    frontfile = getattr(self.charter_img, charterfilename % language, None)
+     # get the frontimage and write it to the canvas
+    charterfilename = "charter_hw2012.jpg"
+    frontfile = getattr(self, charterfilename, None)
     # Fallback to English
-    if not frontfile:
-        frontfile = getattr(self.charter_img, charterfilename % 'en')
-    frontdata = str(frontfile.data)
+#    if not frontfile:
+#        frontfile = getattr(self.charter_img, charterfilename % 'en')
+    frontdata = str(frontfile._data)
     frontfile =StringIO(frontdata)
     frontimage = ImageReader(frontfile)
     # my_canvas.drawImage(frontimage, 0 , 0, 87.5*cm, 123.8*cm)
-    my_canvas.drawImage(frontimage, 0 , 0, 21*cm, 29.7*cm)
+    my_canvas.drawImage(frontimage, 0 , 0, 29.7*cm, 21*cm)
 
     msg_id = 'campaign_name_'+year
     u_campaign_name = translate(
@@ -76,8 +78,9 @@ def generatePDF(self,
                                 target_language=language,
                                 context=self
                                 )
-    x = 10.4 * cm
-    y = 28 * cm
+    x = 14.85 * cm
+    y = 19 * cm
+    my_canvas.setFillColor(HW2012BLUE)
     my_canvas.setFont('ArialBold', 20)
     my_canvas.drawCentredString(x, y, u_campaign_name.upper())
     print " +- set Heading"
@@ -90,10 +93,9 @@ def generatePDF(self,
                                 target_language=language,
                                 context=self
                                 )
-    x = 10.5 * cm
-    y = 26.3 * cm
+    x = 14.85 * cm
+    y = 18 * cm
     my_canvas.setFont('ArialBold', 17)
-    my_canvas.setFillColor(red)
     my_canvas.drawCentredString(x, y, u_campaign_slogan.upper())
     print " +- set slogan"
     mapping = {'campaign_slogan':u_campaign_slogan, 'year':year}
@@ -112,10 +114,10 @@ def generatePDF(self,
                                 target_language=language,
                                 context=self
                                 )
-    x = 10.4 * cm
-    y = 22.5 * cm
+    x = 14.85 * cm
+    y = 11 * cm
     my_canvas.setFont('Arial', 32)
-    my_canvas.setFillColor(black)
+    my_canvas.setFillColor(HW2012BLUE)
     my_canvas.drawCentredString(x, y, certificate_title.upper())
     print " +- set Headline"#, certificate_title
     mapping = {'campaign_slogan':u_campaign_slogan, 'year':year}
@@ -127,16 +129,16 @@ def generatePDF(self,
                                     target_language=language,
                                     context=self)
     certificate_for = certificate_for.encode('utf-8')
-    x = 10.4 * cm
-    y = 21.5 * cm
+    x = 14.85 * cm
+    y = 10 * cm
     my_canvas.setFont('Arial', 18)
     my_canvas.drawCentredString(x, y, certificate_for)
     print " +- set first subline"#, certificate_for
 
     # print company name
-    x = 10.4 * cm
-    y = 16 * cm
-    width, height = A4
+    x = 14.85 * cm
+    y = 7.5 * cm
+    width, height = landscape(A4)
     width -= 5 * cm
     height = 10 * cm
 
@@ -148,9 +150,11 @@ def generatePDF(self,
             alignment=TA_CENTER)
 
     company = saxutils.escape(company)
-    P = Paragraph(company, style)
-    wi, he = P.wrap(width, height)
-    P.drawOn(my_canvas, x - wi/2, y - he/2)
+    my_canvas.setFont('Arial', 32)
+    my_canvas.drawCentredString(x, y, company)
+    # P = Paragraph(company, style)
+    # wi, he = P.wrap(width, height)
+    # P.drawOn(my_canvas, x - wi/2, y - he/2)
     print " +- set company name"#, company
 
     style = ParagraphStyle(
@@ -162,61 +166,43 @@ def generatePDF(self,
             alignment=1
         )
 
-    lines = []
+    #lines = []
+
+
+    # print contribution headline
+    x = 14.85 * cm
+    y = 5 * cm
+    width, height = landscape(A4)
+    width -= 5 * cm
+    height = 10 * cm
+
     contribution_headline = translate(domain=ptt_domain,
-                                          msgid='contribution_headline_'+year,
+                                          msgid='contribution_headline_2012',
                                           mapping=mapping,
                                           target_language=language,
                                           context=self)
 
     contribution_headline = contribution_headline.encode('utf-8')
-    lines.append( Paragraph(contribution_headline, style) )
-    cFrame = Frame(3*cm, 10*cm, 15*cm, 3*cm)
-    cFrame.addFromList(lines, my_canvas)
+    my_canvas.setFont('Arial', 18)
+    my_canvas.drawCentredString(x, y, contribution_headline)
+    # P = Paragraph(contribution_headline, style)
+    # wi, he = P.wrap(width, height)
+    # P.drawOn(my_canvas, x - wi/2, y - he/2)
+    print " +- set contribution headline"#, company
 
-    # Director's signature
-    director_name = "Jukka Takala,"
-    director_name = director_name.encode('utf-8')
-    director_indentifier = translate(domain=ptt_domain,
-                                           msgid='director_'+year,
-                                           mapping=mapping,
-                                           target_language=language,
-                                           context=self)
-    director_indentifier = director_indentifier.encode('utf-8')
-
-    style = ParagraphStyle(
-            name='ContributionHeadline',
-            fontName='Arial',
-            fontSize=16,
-            spaceAfter=6,
-            alignment=0
-        )
-    lines = []
-    lines.append(Paragraph(director_name, style))
-    style = ParagraphStyle(
-            name='ContributionHeadline',
-            fontName='Arial',
-            fontSize=14,
-            spaceAfter=6,
-            alignment=0,
-            leading=16
-        )
-    lines.append(Paragraph(director_indentifier, style))
-    dFrame = Frame(0.8*cm, 5*cm, 20*cm, 3*cm)
-    dFrame.addFromList(lines, my_canvas)
 
 
     # ***** FALLBACK-SOLUTION UNTIL PDFTK IS AVAILABLE *******
     if usePDFTK == 0:
         print "Set Charter-Image as second page w/o PDFTK"
         # set the webcharter-image as second page
-        chartername = "charter_hw2010_%s.jpg"
-        charterfile = getattr(self.charter_img, chartername % language, None)
+        chartername = "charter_hw2012.jpg"
+        charterfile = getattr(self, chartername , None)
         # Fallback to English
-        if not charterfile:
-            charterfile = getattr(self.charter_img, chartername % 'en')
+#        if not charterfile:
+#            charterfile = getattr(self.charter_img, chartername % 'en')
         print "have charterfile:", [charterfile]
-        charterdata = str(charterfile.data)
+        charterdata = str(charterfile._data)
         charterdata = StringIO(charterdata)
         charterimage = ImageReader(charterdata)
         my_canvas.showPage()
@@ -227,17 +213,17 @@ def generatePDF(self,
     if usePDFTK == 1:
         print "Start merging with PDFTK"
         # merge the webcharter and the acknowledge PDFs
-        chartername = "charter_hw2010_%s.jpg"
-        charterfile = getattr(self.charter_img, chartername % language, None)
+        chartername = "charter_hw2012.jpg"
+        charterfile = getattr(self, chartername , None)
         # Fallback to English
-        if not charterfile:
-            charterfile = getattr(self.charter_img, chartername % 'en')
+#        if not charterfile:
+#            charterfile = getattr(self.charter_img, chartername % 'en')
         print "got webcharter:", chartername
 
         # generate temp-files of both PDFs
         tmp_charter_file = tempfile.mkstemp(suffix='.pdf')
         charter_fd = open(tmp_charter_file[1], 'w')
-        charter_fd.write(str(charterfile.data))
+        charter_fd.write(str(charterfile._data))
         #charter_fd.close()
         print "wrote charter-temp", tmp_charter_file[1]
 
