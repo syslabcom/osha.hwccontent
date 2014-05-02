7a423eb14709f0d3671198e3e8477a93f49207f7
Lennart Regebro <regebro@gmail.com>
2014-05-02 20:12:37 +0200

    First attempt at report. #8367

---
+++ src/osha/hwccontent/browser/activity_report.py
@@ -0,0 +1,123 @@
+from DateTime import DateTime
+from Products.Five import BrowserView
+from plone import api
+from StringIO import StringIO
+import csv
+
+def fullname_from_userid(userid):
+    user = api.user.get(userid)
+    return user.fullname or userid
+    
+class ActivityReportView(BrowserView):
+    '''@@activity-report lists all document activity since last report run.'''
+    
+    def __call__(self):
+        catalog = api.portal.get_tool('portal_catalog')
+        workflow = api.portal.get_tool('portal_workflow')
+        types = api.portal.get_tool('portal_types')
+        
+        site_properties = api.portal.get_tool('portal_properties').site_properties
+        if not site_properties.hasProperty('last_activity_report'):
+            site_properties._setProperty('last_activity_report', '', 'string')
+
+        # Find all the organisation folders
+        folders = catalog(portal_type='osha.hwccontent.organisationfolder')
+        paths = [x.getPath() for x in folders]
+
+        query = {'portal_type': ['osha.hwccontent.organisation',
+                                 'osha.hwccontent.focalpoint',
+                                 'osha.hwccontent.mediapartner',
+                                 'Event','Document',
+                                 'Folder',
+                                 'File',
+                                 'News Item',
+                                 ],
+                 'path': paths,
+                 }
+        
+        last_report = site_properties.getProperty('last_activity_report')
+        
+        if last_report:
+            last_report_time = DateTime(last_report)
+            query['modified'] = {'query': last_report_time, 'range': 'min'}
+        else:
+            last_report_time = DateTime(2000, 1, 1, 12, 00)
+
+        results = catalog(**query)      
+
+        # Figure out actions.
+        events = []
+        for result in results:
+            # We have to load objects to check parents and workflow
+            ob = result.getObject()
+
+            # Get the portal type title:
+            portal_type = types[ob.portal_type].title_or_id()
+            
+            # Find the partner:
+            maybe_partner = ob
+            while True:
+                if maybe_partner.portal_type == 'osha.hwccontent.organisation':
+                    partner = maybe_partner.title_or_id()
+                    break
+                maybe_partner = maybe_partner.aq_parent
+                if not hasattr(maybe_partner, 'portal_type'):
+                    # We are outside the site now.
+                    partner = ''
+                    break
+                
+            # URL
+            url = ob.absolute_url()
+            
+            # Find transitions since last report:
+            if hasattr(ob, 'workflow_history'):
+                for wfname, changes in ob.workflow_history.items():
+                    transitions = workflow.getWorkflowById(wfname).transitions
+                    for change in changes:
+                        if change['time'] < last_report_time:
+                            continue
+                        action = change['action']
+                        if not action:
+                            # Initial transition
+                            continue
+                        events.append({
+                            'Partner': partner,
+                            'Event': transitions[action].actbox_name + ' ' + portal_type,
+                            'Date': change['time'].strftime('%Y-%m-%d %H:%M'),
+                            'URL': url,
+                            'Author': fullname_from_userid(change['actor']),
+                        })
+            
+            if ob.created > last_report_time:
+                action = 'New'
+                user = ob.Creator()
+            else:
+                import pdb;pdb.set_trace()
+                action = 'Modified'
+                creators = ob.listCreators()
+                if len(creators) > 1:
+                    # More than one. Let's assume that the last author was not the creator:
+                    creators.remove(doc.Creator())
+                user = creators[-1]
+
+            events.append({
+                'Partner': partner,
+                'Event': action + ' ' + portal_type,
+                'Date': ob.modified().strftime('%Y-%m-%d %H:%M'),
+                'URL': url,
+                'Author': fullname_from_userid(user),
+            })
+        
+        # Update the last report time
+        site_properties._setPropValue('last_activity_report', DateTime().rfc822())
+        
+        # Dump to CSV
+        csvfile = StringIO()
+        writer = csv.DictWriter(csvfile, fieldnames=['Partner', 'Event', 'Date', 'URL', 'Author'])
+        writer.writeheader()
+
+        for event in sorted(events, key=lambda x: x['Partner']):
+            writer.writerow(event)
+            
+        return csvfile.getvalue()
+    
---
+++ src/osha/hwccontent/tests/test_activity_report.py
@@ -0,0 +1,77 @@
+from DateTime import DateTime
+from datetime import datetime, timedelta
+import unittest2 as unittest
+from AccessControl import Unauthorized
+from Products.CMFCore.utils import getToolByName
+from plone import api
+from plone.app.testing import SITE_OWNER_NAME, TEST_USER_NAME
+from plone.app.testing import login, logout
+
+from osha.hwccontent.testing import \
+    OSHA_HWCCONTENT_INTEGRATION_TESTING
+
+
+class TestActivityReport(unittest.TestCase):
+
+    layer = OSHA_HWCCONTENT_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.app = self.layer['app']
+        self.portal = self.layer['portal']
+        self.qi_tool = getToolByName(self.portal, 'portal_quickinstaller')
+
+    def test_report_times(self):
+        """ Make sure that the report only includes changes since last report.
+        """
+        # The report is protected:
+        self.assertRaises(Unauthorized, self.portal.restrictedTraverse, '@@activity-report')
+        
+        login(self.app, SITE_OWNER_NAME)
+        activity_report = self.portal.restrictedTraverse('@@activity-report')
+        
+        # First make a report that we discard that includes any content already
+        # created.
+        result = activity_report()
+        self.assertGreater(len(result.splitlines()), 1) # More than one line
+        
+        # Now set the last report date to the future, and make the report
+        # again, and make sure it is empty:
+        site_properties = api.portal.get_tool('portal_properties').site_properties
+        site_properties._setPropValue('last_activity_report', (DateTime()+1).rfc822())
+
+        result = activity_report()
+        self.assertEqual(result, 
+                         "Partner,Event,Date,URL,Author\r\n") # Only one line, the header
+        
+    def test_report_output(self):
+        # Set up a default workflow
+        wftool = api.portal.get_tool('portal_workflow')
+        wftool.setDefaultChain('plone_workflow')
+
+        # Create some documents and test it.
+        login(self.portal, 'Site Administrator')
+        test_document = api.content.create(container=self.portal.organisations,
+                                           type='Document', id='test_document', 
+                                           title='Test Document')
+        test_organisation = api.content.create(container=self.portal.organisations,
+                                               type='osha.hwccontent.organisation', 
+                                               id='test_organisation', 
+                                               title='Test Organisation')
+        
+        api.content.transition(test_document, 'submit')
+        #login(self.portal, 'Admin')
+        login(self.app, SITE_OWNER_NAME)
+        api.content.transition(test_document, 'publish')
+        
+        activity_report = self.portal.restrictedTraverse('@@activity-report')
+        result = activity_report()
+        
+        # The document:
+        self.assertIn(',New Page,', result) # Note that there is no partner
+        self.assertIn(',http://nohost/plone/organisations/test_document,Site Administrator', result)
+        self.assertIn(',Submit for publication Page,', result)
+        self.assertIn(',http://nohost/plone/organisations/test_document,admin', result)
+        
+        # The organisation:
+        self.assertIn('Test Organisation,New Organisation,', result) # Here we have a partner
+        self.assertIn(',http://nohost/plone/organisations/test_organisation,Site Administrator', result)
