ec3c08ca11faac8763470dc2ce3e50ec0881ba4f
Wolfgang Thomas <thomas@syslab.com>
2014-05-03 16:11:17 +0200

    safeguard against users that are not found Refs #8367

--- src/osha/hwccontent/browser/activity_report.py
+++ src/osha/hwccontent/browser/activity_report.py
@@ -4,19 +4,23 @@ from plone import api
 from StringIO import StringIO
 import csv
 
+
 def fullname_from_userid(userid):
     user = api.user.get(userid)
+    if user is None:
+        return u""
     name = user.fullname or userid
     return name.encode('UTF8')
-    
+
+
 class ActivityReportView(BrowserView):
     '''@@activity-report lists all document activity since last report run.'''
-    
+
     def __call__(self):
         catalog = api.portal.get_tool('portal_catalog')
         workflow = api.portal.get_tool('portal_workflow')
         types = api.portal.get_tool('portal_types')
-        
+
         site_properties = api.portal.get_tool('portal_properties').site_properties
         if not site_properties.hasProperty('last_activity_report'):
             site_properties._setProperty('last_activity_report', '', 'string')
@@ -28,7 +32,7 @@ class ActivityReportView(BrowserView):
         query = {'portal_type': ['osha.hwccontent.organisation',
                                  'osha.hwccontent.focalpoint',
                                  'osha.hwccontent.mediapartner',
-                                 'Event','Document',
+                                 'Event', 'Document',
                                  'Folder',
                                  'File',
                                  'News Item',
@@ -36,16 +40,16 @@ class ActivityReportView(BrowserView):
                  'path': paths,
                  'Language': 'all',
                  }
-        
+
         last_report = site_properties.getProperty('last_activity_report')
-        
+
         if last_report:
             last_report_time = DateTime(last_report)
             query['modified'] = {'query': last_report_time, 'range': 'min'}
         else:
             last_report_time = DateTime(2000, 1, 1, 12, 00)
 
-        results = catalog(**query)      
+        results = catalog(**query)
 
         # Figure out actions.
         events = []
@@ -55,7 +59,7 @@ class ActivityReportView(BrowserView):
 
             # Get the portal type title:
             portal_type = types[ob.portal_type].title_or_id()
-            
+
             # Find the partner:
             maybe_partner = ob
             while True:
@@ -67,11 +71,11 @@ class ActivityReportView(BrowserView):
                     # We are outside the site now.
                     partner = ''
                     break
-            
+
             partner = partner.encode('UTF8')
             # URL
             url = ob.absolute_url()
-            
+
             # Find transitions since last report:
             if hasattr(ob, 'workflow_history'):
                 for wfname, changes in ob.workflow_history.items():
@@ -90,7 +94,7 @@ class ActivityReportView(BrowserView):
                             'URL': url,
                             'Author': fullname_from_userid(change['actor']),
                         })
-            
+
             if ob.created() > last_report_time:
                 action = 'New'
                 user = ob.Creator()
@@ -108,10 +112,10 @@ class ActivityReportView(BrowserView):
                 'URL': url,
                 'Author': fullname_from_userid(user).encode('UTF8'),
             })
-        
+
         # Update the last report time
         site_properties._setPropValue('last_activity_report', DateTime().rfc822())
-        
+
         # Dump to CSV
         csvfile = StringIO()
         writer = csv.DictWriter(csvfile, fieldnames=['Partner', 'Event', 'Date', 'URL', 'Author'])
@@ -127,6 +131,5 @@ class ActivityReportView(BrowserView):
         )
         response.setHeader(
             "Content-Type", 'text/comma-separated-values;charset=utf-8')
-            
+
         return csvfile.getvalue()
-    
