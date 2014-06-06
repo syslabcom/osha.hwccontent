d4d4067e95213616d44239a1e1838250add56dea
Wolfgang Thomas <thomas@syslab.com>
2014-06-06 17:52:08 +0200

    make xls output a bit nicer #9954

--- src/osha/hwccontent/browser/partner_report.py
+++ src/osha/hwccontent/browser/partner_report.py
@@ -2,10 +2,10 @@ from Products.Five import BrowserView
 from plone import api
 from StringIO import StringIO
 from osha.hwccontent.organisation import IOrganisation
-from osha.hwccontent.focalpoint import IFocalPoint
 from osha.hwccontent.mediapartner import IMediaPartner
 from plone.app.textfield.value import RichTextValue
 from zope.schema import getFieldsInOrder
+import xlrd
 import xlwt
 
 
@@ -18,9 +18,9 @@ def fullname_from_userid(userid):
 
 
 class OFMReportViewBase(BrowserView):
-    
+
     portal_types = []
-    report_name ='base'
+    report_name = 'base'
 
     def __call__(self):
         catalog = api.portal.get_tool('portal_catalog')
@@ -30,7 +30,7 @@ class OFMReportViewBase(BrowserView):
         folders = catalog(portal_type='osha.hwccontent.organisationfolder')
         paths = [x.getPath() for x in folders]
 
-        query = {'portal_type': self.portal_types,
+        query = {'portal_type': self.portal_type,
                  'path': paths,
                  'Language': 'all',
                  'sort_on': 'modified',
@@ -38,25 +38,26 @@ class OFMReportViewBase(BrowserView):
 
         field_lists = {
             'osha.hwccontent.organisation': getFieldsInOrder(IOrganisation),
-            'osha.hwccontent.focalpoint': getFieldsInOrder(IFocalPoint),
             'osha.hwccontent.mediapartner': getFieldsInOrder(IMediaPartner),
-            }
-        
-        skip_fields = {'phase_1_intro', 'phase_2_intro', 'privacy_policy_text', 'logo', 'ceo_image'}
+        }
+
+        skip_fields = [
+            'phase_1_intro', 'phase_2_intro', 'privacy_policy_text', 'logo',
+            'ceo_image', 'Description', 'privacy_policy',
+        ]
         fieldnames = []
         fieldtitles = []
-        
-        for fields in field_lists.values():
-            for fieldid, field in fields:
-                if fieldid in skip_fields:
-                    continue
-                if fieldid not in fieldnames:
-                    fieldnames.append(fieldid)
-                    fieldtitles.append(field.title.encode('utf8'))
-        
+
+        for fieldid, field in field_lists[self.portal_type]:
+            if fieldid in skip_fields:
+                continue
+            if fieldid not in fieldnames:
+                fieldnames.append(fieldid)
+                fieldtitles.append(field.title.encode('utf8'))
+
         fieldnames.append('workflow_status')
         fieldtitles.append('Workflow Status')
-                    
+
         results = []
         for partner in catalog(**query):
             ob = partner.getObject()
@@ -66,7 +67,7 @@ class OFMReportViewBase(BrowserView):
                     continue
 
                 v = field.get(ob)
-                
+
                 if fieldid == 'social_media':
                     v = ', '.join('%s: %s' % (x['label'], x['url']) for x in v)
                 elif v is None:
@@ -87,23 +88,42 @@ class OFMReportViewBase(BrowserView):
             p['workflow_status'] = workflow.getTitleForStateOnType(state, ob.portal_type)
             results.append(p)
 
-        # Dump to XLS                
+        # Dump to XLS
         wb = xlwt.Workbook(encoding='utf8')
         ws = wb.add_sheet('Participants')
+        top_xf = xlwt.easyxf('font: bold on; align: wrap on, vert centre, horiz center')
+        normal_xf = xlwt.easyxf('align: wrap off, vert top, horiz left')
         for col, fn in enumerate(fieldtitles):
-            ws.write(0, col, fn)
+            ws.write(0, col, fn, top_xf)
 
         row = 1
         for data in sorted(results, key=lambda x: x['title']):
             for col, fn in enumerate(fieldnames):
-                ws.write(row, col, data.get(fn, ''))
+                ws.write(row, col, data.get(fn, ''), normal_xf)
             row += 1
 
-        xlsfile = StringIO()        
+        buffer = StringIO()
+        wb.save(buffer)
+        read_book = xlrd.open_workbook(file_contents=buffer.getvalue(), formatting_info=True)
+        read_sheet = read_book.sheet_by_index(0)
+        for i in range(read_sheet.ncols):
+            width = 0
+            for j in range(read_sheet.nrows):
+                size = min(len(read_sheet.cell(j, i).value), 50)
+                width = max(width, size)
+            ws.col(i).width = width * 200
+
+        ws.set_remove_splits(True)  # if user does unfreeze, don't leave a split there
+        ws.set_horz_split_pos(1)
+        ws.set_vert_split_pos(0)
+        ws.set_panes_frozen(1)
+
+        xlsfile = StringIO()
         wb.save(xlsfile)
         result = xlsfile.getvalue()
+        buffer.close()
         xlsfile.close()
-        
+
         response = self.request.response
         response.setHeader(
             "Content-Disposition",
@@ -115,17 +135,13 @@ class OFMReportViewBase(BrowserView):
         return result
 
 
-class PartnerReportView(OFMReportViewBase):
-    
-    portal_types =  ['osha.hwccontent.mediapartner',
-                     ]
-    report_name ='mediapartner'
-    
+class MediapartnerReportView(OFMReportViewBase):
 
-class OrgFocReportView(OFMReportViewBase):
+    portal_type = 'osha.hwccontent.mediapartner'
+    report_name = 'mediapartners'
 
-    portal_types =  ['osha.hwccontent.organisation',
-                     'osha.hwccontent.focalpoint',
-                     ]
-    report_name ='organisation-focalpoint'
-    
+
+class OrganisationReportView(OFMReportViewBase):
+
+    portal_type = 'osha.hwccontent.organisation'
+    report_name = 'official-campaign-partners'
