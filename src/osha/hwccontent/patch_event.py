ffd36903d8d1dd432096ca255ec6be0a740b499b
Wolfgang Thomas <thomas@syslab.com>
2014-09-19 10:53:09 +0200

    Event location fetcher also needs to be able to handle JSON results #10682

--- src/osha/hwccontent/patch_event.py
+++ src/osha/hwccontent/patch_event.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.event.browser import event_view
+from plone.app.contenttypes.interfaces import IEvent
 from osha.hwccontent.behaviors.event import IEventLocation
+from osha.hwccontent.interfaces import IEventAccessor
 
 
 def get_location(context):
     """Return the location, using our own field"""
-    data = IEventLocation(context)
-    return data.location
+    location = u""
+    if IEvent.providedBy(context):
+        data = IEventLocation(context)
+        location = data.location
+    elif IEventAccessor.providedBy(context):
+        location = context.location
+    return location
+
 
 event_view.get_location = get_location
